6dbf5f57bec178f86403006ef98907d5b0ab2d0c
D'angelo <dangeloshakur@gmail.com>
2019-03-14 17:17:49 -0500

    D'angelo: Erased everything then got everything back up and running just for Kyle.

--- homework_tracker/settings.py
+++ homework_tracker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'homework_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'homework_tracker',
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASS', 'darquevanella'),
+        'HOST': '127.0.0.1',
+        'PORT': '1033'
     }
 }
 
