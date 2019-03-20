3bf950cca22c078f0884012c43faa8e8c1bb0c98
Kyle Anderson <kyleanderson@Kyles-MacBook-Pro-2.local>
2019-03-20 07:11:16 -0500

    Kyle: can now send a POST request upon submit of profile information, but profile information isn't sending

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('index/', views.index, name='index'),
     path('accounts/signup/', views.signup, name='signup'),
-    path('accounts/create_profile/', views.registration, name='registration'),
-    path('accounts/create_profile', views.CreateProfile, name='create_profile'),
+    path('accounts/register/', views.registration, name='registration'),
+    path('accounts/register/createprofile/', views.CreateProfile, name='create_profile'),
     # path('accounts/registration/<int:user_id>', views.register, name='register'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
